95501b71dd8ebcf36055af0dd02b888458b64ec5
aderemiadequate <temitopeworkspace@gmail.com>
2022-12-12 13:40:14 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - prints the alphabet in lower case
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
- */
+*/
 int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-	putchar('\n');
-	Return (0);
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			putchar(letter);
+
+		putchar('\n');
+
+		return (0);
 }
