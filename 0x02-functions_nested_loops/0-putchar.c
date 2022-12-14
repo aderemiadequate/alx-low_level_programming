ec2e4931c50e1206da9a568c0118e6cb47be3161
aderemiadequate <temitopeworkspace@gmail.com>
2022-12-14 05:14:51 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,17 @@
  * is written by Remy dairo
  * Return: Always 0
 */
+
 int main(void)
 {
-	_putchar('_');
-	_purchar('p');
-	_purchar('u');
-	_purchar('r');
-	_purchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_purchar('p');
+_purchar('u');
+_purchar('r');
+_purchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
