1dc695d6327e590f63352ad1ec636fa5ca5f1c5e
aderemiadequate <temitopeworkspace@gmail.com>
2022-12-14 04:54:04 -0800

    task 0 loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Print _putchar
+ * Return: Always 0
+*/
+int main(void)
+{
+	_putchar('_');
+	_purchar('p');
+	_purchar('u');
+	_purchar('r');
+	_purchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
