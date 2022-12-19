280a1f71c89fc896a1f05c615733c2b27ec43ba3
aderemiadequate <temitopeworkspace@gmail.com>
2022-12-19 15:56:54 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,10 @@ void print_rev(char *s)
 {
 	int a, b;
 
-	b = 0
+	b = 0;
 		while (s[b] != '\0')
 			b++;
-	for  (a = b - 1; a > = 0; a--)
+	for  (a = b - 1; a >= 0; a--)
 	{
 		_putchar(s[a]);
 	}
